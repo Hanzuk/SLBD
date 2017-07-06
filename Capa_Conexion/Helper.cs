65a399c0a79835380e276779bfa4e4c9a0b5e625
Roberto Duarte <robert0d3@outlook.com>
2017-07-06 11:20:08 -0600

    Agregada clase para obtener strings de conexion.

---
+++ Capa_Conexion/Helper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Conexion {
+    public static class Helper {
+        public static string CnnStr(string name) {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
