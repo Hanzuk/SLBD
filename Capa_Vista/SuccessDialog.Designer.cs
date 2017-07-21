8b3e314dea8486c0f10403049cb05b2e42f509df
Roberto Duarte <robert0d3@outlook.com>
2017-07-20 21:56:36 -0600

    Agregado Dialog de exito.

---
+++ Capa_Vista/SuccessDialog.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Capa_Vista {
+    partial class SuccessDialog {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbMessage = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tmOpacity = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(255, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 30);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "¡Éxito!";
+            // 
+            // lbMessage
+            // 
+            this.lbMessage.AutoSize = true;
+            this.lbMessage.Font = new System.Drawing.Font("Segoe UI Semilight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMessage.ForeColor = System.Drawing.Color.White;
+            this.lbMessage.Location = new System.Drawing.Point(104, 222);
+            this.lbMessage.Name = "lbMessage";
+            this.lbMessage.Size = new System.Drawing.Size(0, 28);
+            this.lbMessage.TabIndex = 5;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(177)))), ((int)(((byte)(89)))));
+            this.btnClose.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(196)))), ((int)(((byte)(37)))));
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(191)))), ((int)(((byte)(119)))));
+            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(184)))), ((int)(((byte)(104)))));
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnClose.Location = new System.Drawing.Point(216, 283);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(148, 53);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "OK";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Capa_Vista.Properties.Resources.Success;
+            this.pictureBox1.Location = new System.Drawing.Point(249, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(90, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tmOpacity
+            // 
+            this.tmOpacity.Enabled = true;
+            this.tmOpacity.Interval = 50;
+            this.tmOpacity.Tick += new System.EventHandler(this.tmOpacity_Tick_1);
+            // 
+            // SuccessDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(580, 383);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbMessage);
+            this.Controls.Add(this.btnClose);
+            this.DisplayHeader = false;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Movable = false;
+            this.Name = "SuccessDialog";
+            this.Opacity = 0D;
+            this.Padding = new System.Windows.Forms.Padding(40, 40, 40, 40);
+            this.Resizable = false;
+            this.ShadowType = MetroFramework.Forms.MetroFormShadowType.AeroShadow;
+            this.Style = MetroFramework.MetroColorStyle.Green;
+            this.Text = "¡Éxito!";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.SuccessDialog_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbMessage;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Timer tmOpacity;
+    }
+}
---
+++ Capa_Vista/SuccessDialog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa_Vista {
+    public partial class SuccessDialog : MetroFramework.Forms.MetroForm {
+        public SuccessDialog(string Message) {
+            InitializeComponent();
+            lbMessage.Text = Message;
+        }
+
+        private void btnClose_Click_1(object sender, EventArgs e) {
+            this.Close();
+        }
+
+        public static void ShowDialog(string Message) {
+            SuccessDialog SC = new SuccessDialog(Message);
+            SC.ShowDialog();
+        }
+
+        private void SuccessDialog_Load(object sender, EventArgs e) {
+            tmOpacity.Start();
+        }
+
+        private void tmOpacity_Tick_1(object sender, EventArgs e) {
+            this.Opacity += .20;
+            if (this.Opacity == 1) {
+                tmOpacity.Stop();
+            }
+        }
+    }
+}
