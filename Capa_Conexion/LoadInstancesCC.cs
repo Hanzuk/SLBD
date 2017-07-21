3c3adc1c684bb91fb8c8f4c94ae7c32cb0ab9a65
Roberto Duarte <robert0d3@outlook.com>
2017-07-20 21:55:44 -0600

    Agregado clases para cargar instancias.

---
+++ Capa_Conexion/LoadInstancesCC.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Conexion {
+    public class LoadInstancesCC {
+        public DataTable LoadInstances() {
+            return new Connection(null).LoadInstances();
+        }
+    }
+}
---
+++ Capa_Negocios/LoadInstancesCN.cs
@@ -0,0 +1,24 @@
+﻿using Capa_Conexion;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Negocios {
+    public class LoadInstancesCN {
+        public List<string> LoadInstances() {
+            List<string> ListIntances = new List<string>();
+            foreach (DataRow row in new LoadInstancesCC().LoadInstances().Rows) {
+                string connectionString = row["ServerName"] + "\\" + row["InstanceName"];
+                ListIntances.Add(connectionString);
+            }
+            return ListIntances;
+        }
+
+        public bool TestConnection(string instance) {
+            return new Connection(instance).TestConnection();
+        }
+    }
+}
