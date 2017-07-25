715b4db352cc5b52c5fbcaaca960b80de7828b36
Raidenzero5 <ale05mv@gmail.com>
2017-07-25 11:58:41 -0600

    otros cambios

--- Capa_Negocios/LoadColumnDataCN.cs
+++ Capa_Negocios/LoadColumnDataCN.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 using Capa_Conexion;
 using System.Data;
 
-namespace Capa_Negocios{
+namespace Capa_Negocios {
     public class LoadColumnDataCN {
         public List<string> LoadColumnData(string instance, string dbname, string tbname, string clname){
-            List<string> LoadColumnData = new List<string>();
+            List<string> LoadData = new List<string>();
             foreach (DataRow row in new LoadColumnDataCC().LoadColumnData(instance, dbname, tbname, clname).Rows){
-                LoadColumnData.Add(row[0].ToString());
+                LoadData.Add(row[0].ToString());
             }
-            return LoadColumnData;
+            return LoadData;
         }
     }
 }
