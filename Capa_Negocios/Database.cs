c64f0e0aee80b7a1cad8ef675afcbca60781fa23
Roberto Duarte <robert0d3@outlook.com>
2017-07-29 23:21:15 -0600

    Agregados objetos Table y Database.

---
+++ Capa_Negocios/Database.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Negocios {
+    public class Database {
+        public string Name { get; set; }
+        public string Size { get; set; }
+        public string Owner { get; set; }
+        public string Created { get; set; }
+    }
+}
---
+++ Capa_Negocios/Table.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Negocios {
+    public class Table {
+        public string Schema { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
